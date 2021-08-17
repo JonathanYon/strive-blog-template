1592e567008d06725a33a3a52dde78266b07c139
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-17 18:24:25 +0200

    fetch

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -1,31 +1,62 @@
 import React, { Component } from "react";
-
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 import { Container, Form, Button } from "react-bootstrap";
 import "./styles.css";
+
 export default class NewBlogPost extends Component {
-  // const NewBlogPost = () => {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = {
+      content: "",
+      cover: "linkurl hardcoded",
+      readTime: {
+        value: 2,
+        unit: "minute",
+      },
+      author: {
+        name: "hardCoded",
+        avatar: "hardCoded",
+      },
+    };
+
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(value) {
-    this.setState({ text: value });
+  handleChange(value, bodyContent) {
+    this.setState({
+      ...this.state,
+      [bodyContent]: value,
+    });
+    console.log(value);
   }
+
+  sendForm = async (e) => {
+    e.preventDefault();
+    const res = await fetch(process.env.REACT_APP_URL + "/blogposts", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await res.json();
+    console.log(result);
+  };
 
   render() {
     return (
       <Container className="new-blog-container">
-        <Form className="mt-5">
+        <Form onSubmit={this.sendForm} className="mt-5">
           <Form.Group controlId="blog-form" className="mt-3">
             <Form.Label>Title</Form.Label>
             <Form.Control
               size="lg"
               placeholder="Title"
-              onChange={(e) => this.handeleChange("title", e.target.value)}
+              onChange={(event) =>
+                this.handleChange(event.target.value, "title")
+              }
             />
           </Form.Group>
           <Form.Group controlId="blog-category" className="mt-3">
@@ -33,21 +64,23 @@ export default class NewBlogPost extends Component {
             <Form.Control
               size="lg"
               as="select"
-              onChange={(e) => this.handeleChange("category", e.target.value)}
+              onChange={(event) =>
+                this.handleChange(event.target.value, "category")
+              }
             >
-              <option>Category1</option>
-              <option>Category2</option>
-              <option>Category3</option>
-              <option>Category4</option>
-              <option>Category5</option>
+              <option value="cat1">Category1</option>
+              <option value="cat2">Category2</option>
+              <option value="cat3">Category3</option>
+              <option value="cat4">Category4</option>
+              <option value="cat5">Category5</option>
             </Form.Control>
           </Form.Group>
           <Form.Group controlId="blog-content" className="mt-3">
             <Form.Label>Blog Content</Form.Label>
             <ReactQuill
-              // value={this.state.text}
+              value={this.state.content}
               // onChange={this.handleChange}
-              onChange={(e) => this.handeleChange("content", e.target.value)}
+              onChange={(value) => this.handleChange(value, "content")}
               className="new-blog-content"
             />
           </Form.Group>
@@ -70,4 +103,76 @@ export default class NewBlogPost extends Component {
   }
 }
 
-// export default NewBlogPost;
+// import React, { Component } from "react";
+
+// import "react-quill/dist/quill.snow.css";
+// import ReactQuill from "react-quill";
+// import { Container, Form, Button } from "react-bootstrap";
+// import "./styles.css";
+// export default class NewBlogPost extends Component {
+//   // const NewBlogPost = () => {
+//   constructor(props) {
+//     super(props);
+//     this.state = { text: "" };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
+
+//   handleChange(value) {
+//     this.setState({ text: value });
+//   }
+
+//   render() {
+//     return (
+//       <Container className="new-blog-container">
+//         <Form className="mt-5">
+//           <Form.Group controlId="blog-form" className="mt-3">
+//             <Form.Label>Title</Form.Label>
+//             <Form.Control
+//               size="lg"
+//               placeholder="Title"
+//               onChange={(e) => this.handeleChange("title", e.target.value)}
+//             />
+//           </Form.Group>
+//           <Form.Group controlId="blog-category" className="mt-3">
+//             <Form.Label>Category</Form.Label>
+//             <Form.Control
+//               size="lg"
+//               as="select"
+//               onChange={(e) => this.handeleChange("category", e.target.value)}
+//             >
+//               <option>Category1</option>
+//               <option>Category2</option>
+//               <option>Category3</option>
+//               <option>Category4</option>
+//               <option>Category5</option>
+//             </Form.Control>
+//           </Form.Group>
+//           <Form.Group controlId="blog-content" className="mt-3">
+//             <Form.Label>Blog Content</Form.Label>
+//             <ReactQuill
+//               // value={this.state.text}
+//               // onChange={this.handleChange}
+//               onChange={(e) => this.handeleChange("content", e.target.value)}
+//               className="new-blog-content"
+//             />
+//           </Form.Group>
+//           <Form.Group className="d-flex mt-3 justify-content-end">
+//             <Button type="reset" size="lg" variant="outline-dark">
+//               Reset
+//             </Button>
+//             <Button
+//               type="submit"
+//               size="lg"
+//               variant="dark"
+//               style={{ marginLeft: "1em" }}
+//             >
+//               Submit
+//             </Button>
+//           </Form.Group>
+//         </Form>
+//       </Container>
+//     );
+//   }
+// }
+
+// // export default NewBlogPost;
